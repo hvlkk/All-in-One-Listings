dab241b142b010912d50ddf6d38ae92295bae203
Thanos Trifonis <ath.trifonis@gmail.com>
2023-11-25 17:25:20 +0200

    Add js logic

--- Exercise5/app.js
+++ Exercise5/app.js
@@ -1,3 +1,4 @@
+// Input selectors.
 const form = document.querySelector("form");
 const inEmail = document.getElementById("email-txt");
 const inFirstName = document.getElementById("first-name-txt");
@@ -12,16 +13,44 @@ const inPostCode = document.getElementById("postal-code");
 const inUsername = document.getElementById("username");
 const inPassword = document.getElementById("password");
 const inConfirmPassword = document.getElementById("password-confirmation");
-
+// Button selectors.
 const btnReset = document.querySelector("button.btn-reset");
+const pageSelector = document.querySelector(".pages");
 
+// Form selectors.
+const forms = document.querySelectorAll("form");
+
+// Page selectors.
+const pages = document.querySelector(".pages").children;
+// Page index.
+let pageIndex = 0;
+
+// Fill form with test data.
 fillForm();
+
+// Page Change Event.
+pageSelector.addEventListener("click", (ev) => {
+  const temp = ev.target.getAttribute("data-index");
+  if (temp === pageIndex || temp === undefined || temp === null) {
+    return;
+  }
+  pageIndex = temp;
+  for (let i = 0; i < pages.length; i++) {
+    if (i == pageIndex) {
+      pages[i].classList.add("selected");
+    } else {
+      pages[i].classList.remove("selected");
+    }
+  }
+});
 
 form.addEventListener("submit", (ev) => {
   if (!ev.target.checkValidity()) {
     ev.preventDefault();
     console.log("Form is invalid - prevent submit");
   }
+  ev.preventDefault();
+  console.log("Form is valid - submit");
 });
 
 btnReset.addEventListener("click", (ev) => {
@@ -29,6 +58,29 @@ btnReset.addEventListener("click", (ev) => {
   console.log(inEmail.checkValidity());
 });
 
+inConfirmPassword.addEventListener("input", checkPasswordMatch);
+inPassword.addEventListener("input", checkPasswordMatch);
+
+function checkPasswordMatch() {
+  if (inPassword.value != inConfirmPassword.value) {
+    inConfirmPassword.setCustomValidity("Password mismatch");
+  } else {
+    inConfirmPassword.setCustomValidity("");
+  }
+}
+
+function showForm(index) {
+  for (let i = 0; i < pages.length; i++) {
+    if (i == index) {
+      pages[i].classList.add("selected");
+    } else {
+      forms[i].classLi;
+      pages[i].classList.remove("selected");
+    }
+  }
+}
+
+// Fill form with test data.
 function fillForm() {
   inEmail.value = "papatzopoulos@gmail.com";
   inFirstName.value = "John";
@@ -42,5 +94,5 @@ function fillForm() {
   inPostCode.value = "M1M 1M1";
   inUsername.value = "johnp";
   inPassword.value = "password";
-  inConfirmPassword.value = "password";
+  inConfirmPassword.value = "password1";
 }

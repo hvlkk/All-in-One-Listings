8fb8473c84f2cc43e13e49da0ada7ef40abd5659
Thanos Trifonis <ath.trifonis@gmail.com>
2023-11-25 15:09:40 +0200

    Add input selectors

--- Exercise5/app.js
+++ Exercise5/app.js
@@ -1,28 +1,46 @@
-// document.addEventListener("mouseenter");
-
-function createTooltip() {
-  const tooltip = document.createElement("div");
-  tooltip.classList.add("tooltip");
-  tooltip.textContent = "User Registration";
-
-  tooltip.addEventListener("mouseenter", () => {
-    tooltip.style.opacity = 1;
-  });
-  return tooltip;
-}
-
-const el = document.querySelector("button.btn-reset");
 const form = document.querySelector("form");
+const inEmail = document.getElementById("email-txt");
+const inFirstName = document.getElementById("first-name-txt");
+const inLastName = document.getElementById("last-name-txt");
+const inPhone = document.getElementById("telephone-txt");
+const inDOB = document.getElementById("date-of-birth");
+const inAddress1 = document.getElementById("address-line-1");
+const inAddress2 = document.getElementById("address-line-2");
+const inCity = document.getElementById("city");
+const inCountry = document.getElementById("country");
+const inPostCode = document.getElementById("postal-code");
+const inUsername = document.getElementById("username");
+const inPassword = document.getElementById("password");
+const inConfirmPassword = document.getElementById("password-confirmation");
+
+const btnReset = document.querySelector("button.btn-reset");
+
+fillForm();
 
 form.addEventListener("submit", (ev) => {
-  console.log(ev);
-  ev.preventDefault();
-  console.log("Form submitted");
+  if (!ev.target.checkValidity()) {
+    ev.preventDefault();
+    console.log("Form is invalid - prevent submit");
+  }
 });
 
-el.addEventListener("click", (ev) => {
+btnReset.addEventListener("click", (ev) => {
   ev.preventDefault();
-  console.log(form);
+  console.log(inEmail.checkValidity());
 });
 
-const mailInput = document.getElementById("email-txt");
+function fillForm() {
+  inEmail.value = "papatzopoulos@gmail.com";
+  inFirstName.value = "John";
+  inLastName.value = "Papatzopoulos";
+  inPhone.value = "1234567890";
+  inDOB.value = "1990-01-01";
+  inAddress1.value = "123 Main Street";
+  inAddress2.value = "Apt 1";
+  inCity.value = "Toronto";
+  inCountry.value = "Canada";
+  inPostCode.value = "M1M 1M1";
+  inUsername.value = "johnp";
+  inPassword.value = "password";
+  inConfirmPassword.value = "password";
+}

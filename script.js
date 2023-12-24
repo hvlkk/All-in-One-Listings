99dc7faf283d716e2f6cd49fec2e3f9185ff7090
Thanos Trifonis <ath.trifonis@gmail.com>
2023-12-24 15:25:47 +0200

    Handlebars

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const httpGet = (endpoint) => http("GET", endpoint);
 const httpPost = (endpoint, data) => http("POST", endpoint, data);
 
+const categories = [];
+const name = "Yehuda Katz";
+const people = ["Yehuda Katz", "Alan Johnson", "Charles Jolley"];
+
 async function http(method, endpoint, data = {}) {
   if (method === "GET") {
     const res = await fetch(`https://wiki-ads.onrender.com/${endpoint}`, {
@@ -15,6 +19,17 @@ async function http(method, endpoint, data = {}) {
   return await res.json();
 }
 
+function addSubcategories() {
+  for (let i = 0; i < categories.length; i++) {
+    const category = categories[i];
+    categories[i].subcategories = httpGet(
+      `categories/${category.id}/subcategories`
+    );
+  }
+}
+
 httpGet("categories").then((data) => {
   console.log(data);
 });
+
+let templates = {};

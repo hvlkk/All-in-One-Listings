796f387f447dcf6542c490ce42f4b0fb9f71418d
Thanos Trifonis <ath.trifonis@gmail.com>
2023-12-26 20:12:55 +0200

    login service

--- server.mjs
+++ server.mjs
@@ -8,6 +8,11 @@ const port = 5000;
 
 app.use(cors());
 
+const users = [
+  { id: 1, username: "admin", password: "admin", favourites: [] },
+  { id: 2, username: "user", password: "user", favourites: [] },
+];
+
 app.get("/ads/category", async (req, res) => {
   try {
     const id = req.query.id;
@@ -34,10 +39,30 @@ app.get("/ads/subcategory", async (req, res) => {
 
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
+  const user = loginService(username, password);
+  if (user) {
+    res.json(response(200, "Login Success", user.favourites));
+  } else {
+    res.json(response(401, "Invalid credentials"));
+  }
 });
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
-function loginService(username, password) {}
+function loginService(username, password) {
+  const user = users.find((user) => user.username === username);
+  if (user && user.password === password) {
+    return user;
+  }
+  return null;
+}
+
+function response(code, message, data = null) {
+  return {
+    code: code,
+    message: message,
+    data: data,
+  };
+}
